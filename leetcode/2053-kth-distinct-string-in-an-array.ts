3c4959a1216892e7b4bdeddfabb427a7f77627b0
mariacbsouza <clarabsouza@hotmail.com>
2025-06-17 21:28:00 -0300

    leetcode: 2053

---
+++ leetcode/2053-kth-distinct-string-in-an-array.ts
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode.com/problems/kth-distinct-string-in-an-array/
+ */
+function kthDistinct2(arr: string[], k: number): string {
+  if (!arr || !k) return "";
+
+  const dict: { [word: string]: number } = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+
+    if (dict[current]) {
+      dict[current] += 1;
+      continue;
+    }
+
+    dict[current] = 1;
+  }
+
+  const distinct: string[] = [];
+  for (let [word, occurrences] of Object.entries(dict)) {
+    if (occurrences === 1) {
+      distinct.push(word);
+    }
+  }
+
+  if (distinct.length >= k) {
+    return distinct[k - 1];
+  }
+
+  return "";
+}
