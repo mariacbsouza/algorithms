9a63b31ee71e6982c5fa05aaf3a86ab3c2307d20
mariacbsouza <clarabsouza@hotmail.com>
2025-08-01 13:52:01 -0300

    refactor: improve binary search and change it to ts

--- base/binary-search.ts
+++ base/binary-search.ts
@@ -1,10 +1,4 @@
-/**
- * @param {any[]} list
- * @param {any} target
- * @return {number}
- */
-
-const binarySearch = (list, target) => {
+const binarySearch = (list: number[], target: number) => {
   let start = 0,
     end = list.length - 1,
     mid = 0;
@@ -16,14 +10,14 @@ const binarySearch = (list, target) => {
       return mid;
     }
 
-    if (list[mid] < target) {
-      start = mid + 1;
-    } else {
+    if (list[mid] > target) {
       end = mid - 1;
+    } else {
+      start = mid + 1;
     }
   }
 
-  return -1;
+  return null;
 };
 
 export { binarySearch };
