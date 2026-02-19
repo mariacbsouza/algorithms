2a10428364c55a125ee1b78a1f642f74be031d1f
mariacbsouza <clarabsouza@hotmail.com>
2026-02-19 14:24:11 -0300

    leetcode: 206

---
+++ challenges/206-reverse-linked-list.ts
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+function reverseList2(head: ListNode | null): ListNode | null {
+  let previous = null;
+
+  while (head?.next) {
+    const next = head.next;
+    head.next = previous;
+    previous = head;
+    head = next;
+  }
+
+  if (head) {
+    head.next = previous;
+  }
+
+  return head;
+};
